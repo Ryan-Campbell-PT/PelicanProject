e4c96365980f07fb1513ae148eefb9f9ae80d5ea
Deggserver <56365754+Deggserver@users.noreply.github.com>
2019-10-28 16:25:14 -0500

    Cloned Quin's branch

---
+++ out/production/PelicanProject/src/UtilityClasses/Customer.java
@@ -0,0 +1,101 @@
+package UtilityClasses;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Customer implements Actor
+{
+    private ShoppingCart cart;
+    private String customerId;
+
+    /**
+     * the only thing that should be creating a customer is the signup method, or the login method to retrieve it
+     */
+    private Customer(String username, String password)
+    {
+        ResultSet uniqueCartId;
+        ResultSet uniqueCustomerId;
+        String sql;
+        String createdCustomerId = null; String createdCartId = null;
+        try
+        {
+            do
+            {
+                //these sql commands connect to the database to get a unique cart and customer id
+                sql = "SELECT FLOOR(RAND() * 99999) AS random_num\n" +
+                        "FROM [customerdatabase] \n" +
+                        "WHERE \"random_num\" NOT IN (SELECT customerid FROM [customerdatabase])\n" +
+                        "LIMIT 1";
+                uniqueCustomerId = DatabaseConnection.RunSqlExecuteCommand(sql);
+                sql = "SELECT FLOOR(RAND() * 99999) AS random_num\n" +
+                        "FROM [cartdatabase] \n" +
+                        "WHERE \"random_num\" NOT IN (SELECT cartid FROM [cartdatabase])\n" +
+                        "LIMIT 1";
+                uniqueCartId = DatabaseConnection.RunSqlExecuteCommand(sql);
+            } while (!uniqueCartId.next() && !uniqueCustomerId.next());
+            createdCustomerId = uniqueCustomerId.getString("random_num");
+            createdCartId = uniqueCartId.getString("random_num");
+        }
+        catch(SQLException e) { e.printStackTrace(); }
+
+        sql = "INSERT INTO [customerdatabase] (username, password, uniqueid) values(" + username + ", " + password +
+                " " + createdCustomerId + ");";
+        cart = new ShoppingCart(createdCartId, createdCustomerId);
+    }
+
+    public static Customer getCustomer(String username)
+    {
+        return null;
+    }
+
+    @Override
+    public boolean Login()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean Signup(String username, String password)
+    {
+        String sql = "SELECT username FROM [database] WHERE username = " + username;
+
+        ResultSet resultSet = DatabaseConnection.RunSqlExecuteCommand(sql);
+
+        try
+        {
+            if (!resultSet.next()) //the query returned nothing, so its a valid query
+            {//create a new customer that will add it to the database
+                new Customer(username, password);
+            }
+
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public void ViewAccount()
+    {
+
+    }
+
+    @Override
+    public void UpdateInfo()
+    {
+
+    }
+
+    @Override
+    public void Search(String s)
+    {
+
+    }
+
+    boolean AddToCart(Product p, int quant)
+    {
+        return false;
+    }
+}
---
+++ out/production/PelicanProject/src/UtilityClasses/ProductDetails.java
@@ -0,0 +1,99 @@
+package UtilityClasses;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * I (Ryan) assume that UtilityClasses.ProductDetails will be used as a middle man between the user and the database, where you pass
+ * in the item you want to update, and UtilityClasses.ProductDetails will change the information necessary.
+ * I viewed this as a singleton/static class that would be accessed anywhere in the project to update the item anywhere necessary
+ */
+public class ProductDetails
+{
+    /**
+     * this is the connection to the database that will be used throughout the class
+     */
+    private Connection connection = null;
+
+    private ProductDetails()
+    {
+        // https://docs.oracle.com/javase/tutorial/jdbc/basics/
+        // sql database stuff
+        try
+        {
+            connection = DatabaseConnection.getConnection();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * with the creation of an object, this function will be called to add it to the database,
+     */
+    static void AddProductToDatabase(Product p)
+    {
+        String sqlQuery = "INSERT INTO [table]";
+    }
+
+    /**
+     * this function will be used to add values to the database, for an object that is using
+     * parameters/information that isnt generic to UtilityClasses.Product (id, name, image)
+     * @param productId the productId that will be used to update the information
+     * @param parameterName what the name of the value will be in the database
+     * @param parameterValue the actual information for that value
+     */
+    static void AddParameter(int productId, String parameterName, Object parameterValue)
+    {
+
+    }
+
+    /**
+     * checks the database for this item, and changes any parameters that have changed on it
+     * @param i the object that has all the information, that now wants to be synced with the database
+     */
+    void UpdateItem(Product i)
+    {
+        try
+        {
+            String getObjectSQL = "SELECT * WHERE id EQUALS " + i.getId();
+            PreparedStatement p = connection.prepareStatement(getObjectSQL);
+            ResultSet result = p.executeQuery();
+
+            String itemReceivedFromSQL;
+            while (result.next())
+            {
+                //if done correctly, this loop should only go through once,
+                // and s should contain the info recived from the query
+
+//                itemReceivedFromSQL = result.getString(1);
+
+                i.UpdateProduct(result, connection); //let the object handle the updating
+            }
+
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * assuming every UtilityClasses.DisplayItem has their own tag list, this will return it, in uses like tag searching
+     */
+    ArrayList<String> ShowItemTags(Product i) throws SQLException
+    {
+        //connect to database with this sql statement
+        String getObjectSQL = "SELECT * WHERE id EQUALS " + i.getId();
+        PreparedStatement p = connection.prepareStatement(getObjectSQL);
+        ResultSet result = p.executeQuery();
+
+        String tagsFromSQL;
+        //should only execute once
+        tagsFromSQL = result.getString(1);
+
+        return new ArrayList<>(Arrays.asList(tagsFromSQL.split("whatever the split would be")));
+    }
+}
