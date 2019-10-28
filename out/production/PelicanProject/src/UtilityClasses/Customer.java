9d718fc0cb77662cca886c76aaa8a990a9da9b3c
Deggserver <56365754+Deggserver@users.noreply.github.com>
2019-10-28 16:32:47 -0500

    cloned Quin's branch

--- out/production/PelicanProject/src/UtilityClasses/Customer.java
+++ out/production/PelicanProject/src/UtilityClasses/Customer.java
@@ -6,113 +6,58 @@ import java.sql.SQLException;
 public class Customer implements Actor
 {
     private ShoppingCart cart;
-    private int customerId;
-
-    private String email = null;
-    private String password = null;
-
-    public UserProfile userprofile;
+    private String customerId;
 
     /**
      * the only thing that should be creating a customer is the signup method, or the login method to retrieve it
      */
     private Customer(String username, String password)
     {
-//        ResultSet uniqueCartId;
-//        ResultSet uniqueCustomerId;
-//        String createdCustomerId = null;
-//        String createdCartId = null;
+        ResultSet uniqueCartId;
+        ResultSet uniqueCustomerId;
         String sql;
-        ResultSet sql_resultset;
-        boolean sql_result;
-//        try
-//        {
-//            do
-//            {
+        String createdCustomerId = null; String createdCartId = null;
+        try
+        {
+            do
+            {
                 //these sql commands connect to the database to get a unique cart and customer id
-//                sql = "SELECT FLOOR(RAND() * 99999) AS random_num\n" +
-//                        "FROM user_information \n" +
-//                        "WHERE \"random_num\" NOT IN (SELECT user_id FROM user_information)\n" +
-//                        "LIMIT 1";
-//                uniqueCustomerId = DatabaseConnection.RunSqlExecuteCommand(sql);
-//                sql = "SELECT FLOOR(RAND() * 99999) AS random_num\n" +
-//                        "FROM [cartdatabase] \n" +
-//                        "WHERE \"random_num\" NOT IN (SELECT cartid FROM [cartdatabase])\n" +
-//                        "LIMIT 1";
-//                uniqueCartId = DatabaseConnection.RunSqlExecuteCommand(sql);
-//            } while (!uniqueCartId.next() && !uniqueCustomerId.next());
-//            createdCustomerId = uniqueCustomerId.getString("random_num");
-//            createdCartId = uniqueCartId.getString("random_num");
-//        }
-//        catch(SQLException e) { e.printStackTrace(); }
-
-        sql = "INSERT INTO user_information (e_addr, pass) values('" + username + "', '" + password + "');";
-        sql_result = DatabaseConnection.RunSqlCreateCommand(sql);
-        try {
-            if(sql_result){
-                sql = "SELECT * FROM user_information WHERE e_addr='" + username + "';";
-                sql_resultset = DatabaseConnection.RunSqlExecuteCommand(sql);
-                if(sql_resultset.next()) {
-                    this.customerId = sql_resultset.getInt("user_id");
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+                sql = "SELECT FLOOR(RAND() * 99999) AS random_num\n" +
+                        "FROM [customerdatabase] \n" +
+                        "WHERE \"random_num\" NOT IN (SELECT customerid FROM [customerdatabase])\n" +
+                        "LIMIT 1";
+                uniqueCustomerId = DatabaseConnection.RunSqlExecuteCommand(sql);
+                sql = "SELECT FLOOR(RAND() * 99999) AS random_num\n" +
+                        "FROM [cartdatabase] \n" +
+                        "WHERE \"random_num\" NOT IN (SELECT cartid FROM [cartdatabase])\n" +
+                        "LIMIT 1";
+                uniqueCartId = DatabaseConnection.RunSqlExecuteCommand(sql);
+            } while (!uniqueCartId.next() && !uniqueCustomerId.next());
+            createdCustomerId = uniqueCustomerId.getString("random_num");
+            createdCartId = uniqueCartId.getString("random_num");
         }
-//        cart = new ShoppingCart(createdCartId, createdCustomerId);
-    }
-    private Customer(String email_arg){
-        String sql;
-        ResultSet sql_resultset;
-        sql = String.format("SELECT * FROM user_information " +
-                "WHERE e_addr = '%s'", email_arg);
-        sql_resultset = DatabaseConnection.RunSqlExecuteCommand(sql);
-        try {
-            if(sql_resultset.next()){
-                this.customerId = sql_resultset.getInt("user_id");
-                this.email = sql_resultset.getString("e_addr");
-                this.password = sql_resultset.getString("pass");
-                this.userprofile = new UserProfile();
-                ViewAccount();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        catch(SQLException e) { e.printStackTrace(); }
 
+        sql = "INSERT INTO [customerdatabase] (username, password, uniqueid) values(" + username + ", " + password +
+                " " + createdCustomerId + ");";
+        cart = new ShoppingCart(createdCartId, createdCustomerId);
     }
 
-    public static Customer getCustomer(String email_arg)
+    public static Customer getCustomer(String username)
     {
-        Customer getC = new Customer(email_arg);
-        return getC;
+        return null;
     }
 
     @Override
     public boolean Login()
     {
-        boolean result = false;
-        ResultSet sql_resultset;
-        String sql = String.format("SELECT user_id FROM user_information " +
-                "WHERE e_addr='%s' AND pass = '%s';" , this.email , this.password);
-        sql_resultset = DatabaseConnection.RunSqlExecuteCommand(sql);
-        try {
-            if(sql_resultset.next()){
-                if(sql_resultset.getInt("user_id") == this.customerId) {
-                    result = true;
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
-            return result;
-        }
+        return false;
     }
 
-    @SuppressWarnings("SpellCheckingInspection")
-    public static boolean Signup(String username, String password)
+    @Override
+    public boolean Signup(String username, String password)
     {
-        String sql = "SELECT * FROM user_information WHERE e_addr = '" + username + "'";
+        String sql = "SELECT username FROM [database] WHERE username = " + username;
 
         ResultSet resultSet = DatabaseConnection.RunSqlExecuteCommand(sql);
 
@@ -121,7 +66,6 @@ public class Customer implements Actor
             if (!resultSet.next()) //the query returned nothing, so its a valid query
             {//create a new customer that will add it to the database
                 new Customer(username, password);
-                return true;
             }
 
         }
@@ -135,13 +79,13 @@ public class Customer implements Actor
     @Override
     public void ViewAccount()
     {
-        this.userprofile.getUserInfo(this.email);
+
     }
 
     @Override
-    public void UpdateInfo(String[] args)
+    public void UpdateInfo()
     {
-        this.userprofile.setUserInfo(this.email , args);
+
     }
 
     @Override
@@ -153,13 +97,5 @@ public class Customer implements Actor
     boolean AddToCart(Product p, int quant)
     {
         return false;
-    }
-
-    public int getCustomerId() {
-        return this.customerId;
-    }
-
-    public String getEmail(){
-        return this.email;
     }
 }
