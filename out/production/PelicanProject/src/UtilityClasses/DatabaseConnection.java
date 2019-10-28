a3cc66b76f9d9244819add269bfd25171b85ce4b
Ditone <qcreidy@gmail.com>
2019-10-28 16:36:51 -0500

    Added Instruction interface.

---
+++ out/production/PelicanProject/src/UtilityClasses/DatabaseConnection.java
@@ -0,0 +1,93 @@
+package UtilityClasses;
+
+import java.sql.*;
+
+/**
+ * this class will be used exclusively as a way to connect to the database, to give one connection to it
+ * to make our lives easier when trying to modify
+ */
+public class DatabaseConnection
+{
+    private static Connection conn = null;
+
+    private static final String DB_URL = "jdbc:mysql://3.14.159.254:3306/software_projects";
+    //sets up the connection to our private Oracle Autonomous Database. Will later need to be updated to handle user name + password entry
+
+    //Must be run before each SQL command / start of instance
+    protected static Connection getConnection()
+    {
+        if(conn == null)
+        {
+            try
+            {
+                System.out.println ("Finding driver");
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                System.out.println ("Connecting to the Database");
+                conn = DriverManager.getConnection(DB_URL, "qreidy", "Pelican");
+                System.out.println ("Connected");
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return conn;
+    }
+
+    public static ResultSet RunSqlExecuteCommand(String sql)
+    {
+        ResultSet result = null;
+        try
+        {
+            PreparedStatement p = getConnection().prepareStatement(sql);
+            result = p.executeQuery();
+        }
+
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        //for updates/deletes, expected result is an empty result set
+        return result;
+    }
+
+    /**
+     * running a sql command to get something back, like select * from database,
+     * is different than running something to create something, like create table ding,
+     * so this will be used to allow for creation commands, and the above is to get something back
+     * @param sql
+     * @return whether it succeeded
+     */
+    public static boolean RunSqlCreateCommand(String sql)
+    {
+        boolean ret;
+        try
+        {
+            PreparedStatement p = getConnection().prepareStatement(sql);
+            p.execute(sql);
+            ret = true;
+        }
+
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            ret = false;
+        }
+
+        return ret;
+    }
+
+    //Must be run after connection is done being used
+    public static void CloseConnection(){
+        try {
+            conn.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ out/production/PelicanProject/src/UtilityClasses/ShoppingCart.java
@@ -0,0 +1,44 @@
+package UtilityClasses;
+
+import javafx.util.Pair;
+import java.util.List;
+
+
+public class ShoppingCart
+{
+    private String uniqueCartId; //every cart will have their own id to recognize it in the db
+    private String uniqueCustomerId; //every cart will have the customer id associated with their customer
+    private List < Pair <Product, Integer> > cart;
+
+    protected ShoppingCart(String cartId, String customerId)
+    {
+        this.uniqueCartId = cartId;
+        this.uniqueCustomerId = customerId;
+    }
+
+    public boolean addToCart(Product p, Integer i){
+        try {
+            this.cart.add(new Pair<>(p, i));
+            return true;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List < Pair <Product, Integer>  > getCart(){
+        return this.cart;
+    }
+
+    public boolean emptyCart(){
+        try {
+            this.cart = null;
+            return true;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
