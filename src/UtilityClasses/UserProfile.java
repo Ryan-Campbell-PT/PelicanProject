8c94085c58a14207ebfccc75793702fb9622cb3c
Deggserver <56365754+Deggserver@users.noreply.github.com>
2019-10-29 20:15:37 -0500

    Added framework for the rest of the file and soft implemented the getUserImage() function to return server-based image depending on if that user exists or not.

--- src/UtilityClasses/UserProfile.java
+++ src/UtilityClasses/UserProfile.java
@@ -5,28 +5,23 @@ import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 
 /**
  * This class is to facilitate getting information about the user for us to use in the user
  * profile page.
  */
 public class UserProfile{
-    final String PATH_TO_SHARE = "eggserver.info/public/";
-    public Image getUserImage(String user_id){
+    public Image getUserImage(String user_email){
         Image icon = null;
         URL url;
         String sql;
         ResultSet sql_result;
 
-        sql = "SELECT u_icon FROM user_information WHERE u_id=" + user_id + ";";
+        sql = "SELECT u_id , u_icon FROM software_project.user_information WHERE u_email EQUALS " + user_email + ";";
         sql_result = DatabaseConnection.RunSqlExecuteCommand(sql);
 
         if(sql_result == null) {
-            /*
-            User image not found, given default image
-            */
+//          User image not found, given default image
             try {
                 url = new URL("eggserver.info/public/default_user_image.png");
                 icon = ImageIO.read(url);
@@ -41,13 +36,6 @@ public class UserProfile{
              */
             try {
                 //spec is currently not really implemented, just placeholder.
-                String user_icon_path = PATH_TO_SHARE;
-                try {
-                    sql_result.next();
-                    user_icon_path.concat(sql_result.getString("u_icon"));
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
                 url = new URL("eggserver.info/public/some_user_image.png");
                 icon = ImageIO.read(url);
             } catch (IOException e1) {
@@ -59,64 +47,28 @@ public class UserProfile{
 
     /**
      * @TODO
-     * @param user_id 4head
+     * @param user_email 4head
      * @return If the query found a user, return true. Else the user does not exist and return false.
      */
-    public boolean setUserImage(String user_id){
+    public boolean setUserImage(String user_email){
         return false;
     }
 
     /**
-     *
-     * @param user_id 4head
-     * @return  User's data in an array for ease of manipulation, if null no user exists
+     * @TODO
+     * @param user_email 4head
+     * @return  User's data in an array for ease of manipulation
      */
-    public String[][] getUserInfo(String user_id) throws SQLException {
-        String sql;
-        ResultSet sql_result;
-
-        sql = "SELECT * FROM user_information WHERE user_id=" + user_id + ";";
-        sql_result = DatabaseConnection.RunSqlExecuteCommand(sql);
-
-        if(sql_result == null) {
-            /*
-            User not found, return null
-            */
-            return null;
-        }
-        else{
-            /*
-            User found, returning their information.
-            Get user's info which is found by the query on user_information matching e_address == user_id
-             */
-            ArrayList<String[]> user_information = new ArrayList<String[]>(); //This is the array that will end up being returned.
-            sql_result.next(); //This gets the result set to the 1st row of the query. The result set starts before the 1st row of data.
-
-            //get all data for row (This looks horrible and needs to be cleaned up...)
-            user_information.add(new String[]{"user_id" , Integer.toString(sql_result.getInt("user_id"))});
-            user_information.add(new String[]{"f_name" , sql_result.getString("f_name")});
-            user_information.add(new String[]{"l_name" , sql_result.getString("l_name")});
-            user_information.add(new String[]{"p_addr" , sql_result.getString("p_addr")});
-            user_information.add(new String[]{"e_addr" , sql_result.getString("e_addr")});
-            user_information.add(new String[]{"pass" , sql_result.getString("pass")});
-            user_information.add(new String[]{"a_level" , Integer.toString(sql_result.getInt("a_level"))});
-            user_information.add(new String[]{"u_icon" , sql_result.getString("u_icon")});
-/*
-            //Test that the query and extraction worked
-            for(int i = 0; i < user_information.size(); i++){
-                System.out.println(user_information.get(i)[0] + " " + user_information.get(i)[1]);
-            }
-*/
-            return user_information.toArray(new String[user_information.size()][2]); //Cast to fix the '.toArray()' function returning an object of type 'Object'
-        }
+    public String[] getUserInfo(String user_email){
+        return null;
     }
 
     /**
      * @TODO
-     * @param user_id 4 h e a d
-     * @return If the query found a user and was able to update database, return true. Else the user does not exist || update unsuccessful and return false.
+     * @param user_email 4 h e a d
+     * @return If the query found a user, return true. Else the user does not exist and return false.
      */
-    public boolean setUserInfo(String user_id){
+    public boolean setUserInfo(String user_email){
         return false;
     }
 }
