065a7ceaa75831f26ef7691fd9c45dcfc4ecae4a
tiber39 <50217393+tiber39@users.noreply.github.com>
2019-11-11 13:56:09 -0600

    Adding Junit officially to classpath and libraries

---
+++ src/JunitTestSuites/ValidLoginTests.java
@@ -0,0 +1,13 @@
+package JunitTestSuites;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidLoginTests {
+
+    @Test
+    public void firstTest() {
+        Assert.assertTrue(true);
+
+    }
+}
+
